5e36f1d7fde0311737edee72046267b96b3be001
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-03-20 20:20:24 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,27 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = file_name.with_suffix('.txt')
+    try:
+        with open(file_path, 'w') as file:
+            file.write(file_content)
+        print(f'File "{file_path}" written successfully.')
+    except Exception as e:
+        print(f'Error writing file: {e}')
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, file_content):
+    file_path = file_name.with_suffix('.txt')
+    try:
+        with open(file_path, 'a') as file:
+            file.write(file_content)
+        print(f'Content appended to file "{file_path}" successfully.')
+    except Exception as e:
+        print(f'Error appending to file: {e}')
 
 def read_file(file_name):
-    pass
+    file_path = file_name.with_suffix('.txt')
+    try:
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+        return file_content
+    except Exception as e:
+        print(f'Error reading file: {e}')
+        return None
